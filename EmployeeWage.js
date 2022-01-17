6e370259cee2922431dec71876ce84b3996680cb
ssaini07 <78027352+ssaini07@users.noreply.github.com>
2022-01-17 19:35:55 +0530

    Added : calculated employee daily working hours

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -3,18 +3,18 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOUR = 4;
 const FULL_TIME_HOUR = 8;
 const WAGE_PER_HOUR = 20;
-let empHrs = 0;
-let empCheck = Math.floor(Math.random() * 10) % 3;
-switch (empCheck) {
-    case IS_PART_TIME:
-        empHrs = PART_TIME_HOUR;
-        break;
-    case IS_FULL_TIME:
-        empHrs = FULL_TIME_HOUR;
-        break;
-    default:
-        empHrs = 0;
-
+//let empHrs = 0;
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOUR;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOUR;
+        default:
+            empHrs = 0;
+    }
 }
+let empCheck = Math.floor(Math.random() * 10) % 3;
+empHrs = getWorkingHours(empCheck);
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Employee Wage:" + empWage);
+console.log(" Emp Hour:" + empHrs + " Emp Wage: " + empWage);
