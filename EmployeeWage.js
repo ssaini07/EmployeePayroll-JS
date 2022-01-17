01d1c7ada2d83caa8c137ad8f716404844bcad71
ssaini07 <78027352+ssaini07@users.noreply.github.com>
2022-01-17 21:43:24 +0530

    Added : Calculated employee total days, total hours and total wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -4,6 +4,7 @@ const PART_TIME_HOUR = 4;
 const FULL_TIME_HOUR = 8;
 const WAGE_PER_HOUR = 20;
 const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 160;
 function getWorkingHours(empCheck) {
     switch (empCheck) {
         case IS_PART_TIME:
@@ -14,10 +15,13 @@ function getWorkingHours(empCheck) {
             empHrs = 0;
     }
 }
-let empHrs = 0;
-for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
-    empHrs += getWorkingHours(empCheck);
+    totalEmpHrs += getWorkingHours(empCheck);
 }
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log(" Total Hours: " + empHrs + " Emp Wage: " + empWage);
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("UC5 -Total Days: " + totalWorkingDays + " Total Hours: " + totalEmpHrs + " Employee Wage:" + empWage);
