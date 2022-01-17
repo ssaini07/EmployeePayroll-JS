33c584a108935981fc2f0a355853375fa451dbb8
ssaini07 <78027352+ssaini07@users.noreply.github.com>
2022-01-17 22:12:50 +0530

    Added : Store the daily wage along with the total wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,27 +1,35 @@
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
-const PART_TIME_HOUR = 4;
-const FULL_TIME_HOUR = 8;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 const NUM_OF_WORKING_DAYS = 20;
 const MAX_HRS_IN_MONTH = 160;
+let empDailyWageArr = new Array();
+
 function getWorkingHours(empCheck) {
     switch (empCheck) {
         case IS_PART_TIME:
-            return PART_TIME_HOUR;
+            return PART_TIME_HOURS;
         case IS_FULL_TIME:
-            return FULL_TIME_HOUR;
+            return FULL_TIME_HOURS;
         default:
-            empHrs = 0;
+            return 0;
     }
 }
 
+/** function for calculating daily wage */
+function calculateDailyWage(empHrs) {
+    return empHrs * WAGE_PER_HOUR;
+}
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
 while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
     totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
-    totalEmpHrs += getWorkingHours(empCheck);
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyWageArr.push(calculateDailyWage(empHrs));
 }
 let empWage = totalEmpHrs * WAGE_PER_HOUR;
-console.log("UC5 -Total Days: " + totalWorkingDays + " Total Hours: " + totalEmpHrs + " Employee Wage:" + empWage);
+console.log("UC6 -Total Days:" + totalWorkingDays + "," + " Total Hours:" + totalEmpHrs + "," + " Employee Wage: " + empWage + "," + "Daily Wages: " + "[" + empDailyWageArr + "]");
