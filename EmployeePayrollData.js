2e6b013c6e19f3886a7f46a41f7402469472aa98
ssaini07 <78027352+ssaini07@users.noreply.github.com>
2022-01-19 22:24:54 +0530

    Added : check whether name starts with capital and has atleast 3 characters

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -1,12 +1,12 @@
 class EmployeePayrollData {
     id;
-    name;
+    //name;
     salary;
     gender;
     startDate;
-    constructor(id, name, salary, gender, startDate) {
+    constructor(id, salary, gender, startDate) {
         this.id = id;
-        this.name = name;
+        //this.name = name;
         this.salary = salary;
         this.gender = gender;
         this.startDate = startDate;
@@ -18,21 +18,31 @@ class EmployeePayrollData {
     }
 
     set name(name) {
-        console.log("Inside setter");
-        this._name = name;
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+        if (nameRegex.test(name)) {
+            this._name = name;
+        }
+        else {
+            throw "Sorry name is incorrect";
+        }
     }
-//Method
+    //Method
     toString() {
-        return " id = " + this.id + ", name = " + this.name + ", salary = " + this.salary  + " gender = "+ 
-        this.gender + " startDate = " + this.startDate;
+        return " id = " + this.id + ", name = " + this.name + ", salary = " + this.salary + " gender = " +
+            this.gender + " startDate = " + this.startDate;
     }
 }
 
 let employeePayrollData = new EmployeePayrollData(1, "Shubham", 500000);
-console.log(employeePayrollData);
-employeePayrollData.name = "SAINI";
-console.log(employeePayrollData);
-console.log(employeePayrollData.name);
-
-let newEmployeePayrollData = new EmployeePayrollData(2, "Karanveer", 600000, "M", new Date());
-console.log(newEmployeePayrollData.toString());
+//console.log(employeePayrollData);
+//employeePayrollData.name = "SAINI";
+//console.log(employeePayrollData);
+//console.log(employeePayrollData.name);
+try {
+    employeePayrollData.name = "Saini123";
+    console.log(employeePayrollData);
+} catch (e) {
+    console.log(e);
+}
+//let newEmployeePayrollData = new EmployeePayrollData(2, "Karanveer", 600000, "M", new Date());
+//console.log(newEmployeePayrollData.toString());
