57e38308a23bc7f6de1fbd6ad5f62667e7d97cf7
ssaini07 <78027352+ssaini07@users.noreply.github.com>
2022-01-19 22:08:25 +0530

    Added : Stored gender nd start date

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -1,11 +1,15 @@
 class EmployeePayrollData {
     id;
-    //name;
+    name;
     salary;
-    constructor(id, name, salary) {
+    gender;
+    startDate;
+    constructor(id, name, salary, gender, startDate) {
         this.id = id;
-        // this.name = name;
+        this.name = name;
         this.salary = salary;
+        this.gender = gender;
+        this.startDate = startDate;
     }
 
     get name() {
@@ -17,6 +21,11 @@ class EmployeePayrollData {
         console.log("Inside setter");
         this._name = name;
     }
+//Method
+    toString() {
+        return " id = " + this.id + ", name = " + this.name + ", salary = " + this.salary  + " gender = "+ 
+        this.gender + " startDate = " + this.startDate;
+    }
 }
 
 let employeePayrollData = new EmployeePayrollData(1, "Shubham", 500000);
@@ -24,3 +33,6 @@ console.log(employeePayrollData);
 employeePayrollData.name = "SAINI";
 console.log(employeePayrollData);
 console.log(employeePayrollData.name);
+
+let newEmployeePayrollData = new EmployeePayrollData(2, "Karanveer", 600000, "M", new Date());
+console.log(newEmployeePayrollData.toString());
